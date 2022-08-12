2896f7779140129def846b2bad3c44aa5930f948
newclarityex <69740123+newclarityex@users.noreply.github.com>
2022-08-12 04:32:28 -0400

    set up supabase

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
-  let count: number = 0
-  const increment = () => {
-    count += 1
-  }
+    import { onMount } from "svelte";
+    import { supabase } from "../supabaseClient";
+    onMount(async () => {
+        let { data, error } = await supabase
+            .from("words")
+            .select("text,length")
+            .limit(100);
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(data);
+        }
+    });
+
+    let count: number = 0;
+    const increment = () => {
+        count += 1;
+    };
 </script>
 
 <button on:click={increment}>
-  count is {count}
+    count is {count}
 </button>
